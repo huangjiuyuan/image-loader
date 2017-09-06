72add847b37b9d8ab4ad48ce4a4edce46200de2d
huangjiuyuan <jiuyuan.huang@daocloud.io>
2017-09-06 14:15:43 +0800

    Dockerfile.conformance added

---
+++ version.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import logging
+import re
+import os
+
+
+logging.debug('Setting the target k8s version')
+kver = os.getenv('KVER')
+version_pattern = re.compile('v[0-9]+.[0-9]+.[0-9]+')
+fname = 'Makefile'
+with open(fname) as f:
+    tmp = fname + '.tmp'
+    out = open(tmp, 'w')
+    for line in f:
+        out.write(re.sub(version_pattern, kver, line))
+    out.close()
+    os.rename(tmp, fname)
